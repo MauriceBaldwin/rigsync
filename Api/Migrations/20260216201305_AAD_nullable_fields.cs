ecbc5190f003cd5d6ec5fc02cb954ded3d1b333d
Maurice Baldwin <baldwinmb13@gmail.com>
2026-02-16 20:30:31 +0000

    fix formatting warnings

--- Api/Migrations/20260216201305_AAD_nullable_fields.cs
+++ Api/Migrations/20260216201305_AAD_nullable_fields.cs
@@ -1,55 +1,56 @@
-﻿using System;
+﻿// RigSync | 20260216201305_AAD_nullable_fields.cs
+
+namespace Api.Migrations;
+
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Api.Migrations
+/// <inheritdoc />
+public partial class AAD_nullable_fields : Migration
 {
-    /// <inheritdoc />
-    public partial class AAD_nullable_fields : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<DateOnly>(
-                name: "NextServiceDue",
-                table: "AADs",
-                type: "date",
-                nullable: true,
-                oldClrType: typeof(DateOnly),
-                oldType: "date");
+  /// <inheritdoc />
+  protected override void Up(MigrationBuilder migrationBuilder)
+  {
+    migrationBuilder.AlterColumn<DateOnly>(
+        name: "NextServiceDue",
+        table: "AADs",
+        type: "date",
+        nullable: true,
+        oldClrType: typeof(DateOnly),
+        oldType: "date");
 
-            migrationBuilder.AlterColumn<DateOnly>(
-                name: "EndOfLife",
-                table: "AADs",
-                type: "date",
-                nullable: true,
-                oldClrType: typeof(DateOnly),
-                oldType: "date");
-        }
+    migrationBuilder.AlterColumn<DateOnly>(
+        name: "EndOfLife",
+        table: "AADs",
+        type: "date",
+        nullable: true,
+        oldClrType: typeof(DateOnly),
+        oldType: "date");
+  }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<DateOnly>(
-                name: "NextServiceDue",
-                table: "AADs",
-                type: "date",
-                nullable: false,
-                defaultValue: new DateOnly(1, 1, 1),
-                oldClrType: typeof(DateOnly),
-                oldType: "date",
-                oldNullable: true);
+  /// <inheritdoc />
+  protected override void Down(MigrationBuilder migrationBuilder)
+  {
+    migrationBuilder.AlterColumn<DateOnly>(
+        name: "NextServiceDue",
+        table: "AADs",
+        type: "date",
+        nullable: false,
+        defaultValue: new DateOnly(1, 1, 1),
+        oldClrType: typeof(DateOnly),
+        oldType: "date",
+        oldNullable: true);
 
-            migrationBuilder.AlterColumn<DateOnly>(
-                name: "EndOfLife",
-                table: "AADs",
-                type: "date",
-                nullable: false,
-                defaultValue: new DateOnly(1, 1, 1),
-                oldClrType: typeof(DateOnly),
-                oldType: "date",
-                oldNullable: true);
-        }
-    }
+    migrationBuilder.AlterColumn<DateOnly>(
+        name: "EndOfLife",
+        table: "AADs",
+        type: "date",
+        nullable: false,
+        defaultValue: new DateOnly(1, 1, 1),
+        oldClrType: typeof(DateOnly),
+        oldType: "date",
+        oldNullable: true);
+  }
 }
