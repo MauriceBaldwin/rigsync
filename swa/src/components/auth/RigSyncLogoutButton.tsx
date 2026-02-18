308b60eeee4fa141c245de8409ac8dd7ad64a6d8
Maurice Baldwin <baldwinmb13@gmail.com>
2026-02-18 22:55:31 +0000

    loading state on logout button

--- swa/src/components/auth/RigSyncLogoutButton.tsx
+++ swa/src/components/auth/RigSyncLogoutButton.tsx
@@ -1,18 +1,23 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Button } from '@mui/material';
 import RigSyncAuthContext from '../../context/RigSyncAuthContext';
 
 const RigSyncLogoutButton = () => {
   const authContext = useContext(RigSyncAuthContext);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const logout = () => {
+    setIsLoading(true);
+    void authContext?.logout();
+  };
 
   return (
     <Button
       variant="outlined"
       color="primary"
       size="large"
-      onClick={() => {
-        void authContext?.logout();
-      }}
+      loading={isLoading}
+      onClick={logout}
     >
       Logout
     </Button>
